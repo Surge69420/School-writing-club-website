d707956446635606ebc19bc929fc829ffb9f70da
Surge69420 <stormbreakerxtno1@gmail.com>
2024-04-11 15:00:46 +0100

    made the buttons actually functional so you can switch between pages

---
+++ index.js
@@ -0,0 +1,10 @@
+if(document.getElementById("Poemas") != null){
+    document.getElementById("Poemas").addEventListener("click", () => {
+        window.location.href = "poemas.html";
+    })
+}
+if(document.getElementById("back") != null){
+    document.getElementById("back").addEventListener("click", () => {
+        window.location.href = "index.html";
+    })
+}
