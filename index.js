4591e9f479005d6c8052cca7ae383108b9d3e388
Surge69420 <stormbreakerxtno1@gmail.com>
2024-04-11 15:11:47 +0100

    js some formatting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-if(document.getElementById("Poemas") != null){
+if (document.getElementById("Poemas") != null) {
     document.getElementById("Poemas").addEventListener("click", () => {
         document.getElementById('coverwholescreenUn').classList.add('coverwholescreen');
-        setTimeout(function() {
+        setTimeout(function () {
             // Redirect to a different HTML page
             window.location.href = 'Poemas.html';
         }, 1000);
     })
 }
-if(document.getElementById("back") != null){
+if (document.getElementById("back") != null) {
     document.getElementById("back").addEventListener("click", () => {
         document.getElementById('coverwholescreenUn').classList.add('coverwholescreen');
-        setTimeout(function() {
+        setTimeout(function () {
             // Redirect to a different HTML page
             window.location.href = 'index.html';
         }, 1000);
